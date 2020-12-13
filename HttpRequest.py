464830ab214feb51e879f9173639923dea5d56c8
Denis <kolpashikov.denis@gmail.com>
2020-12-13 22:25:18 +0400

    Изменён фильтр, добавлен выбор таблиц

--- HttpRequest.py
+++ HttpRequest.py
@@ -16,12 +16,17 @@ password = config['DEFAULT']['password']
 str_date = config['DEFAULT']['date']
 is_entity = bool(config['DEFAULT']['entity'])
 is_entity_route_sheet = bool(config['DEFAULT']['entity_route_sheet'])
-
+is_entity_route_sheet_operation = bool(config['DEFAULT']['entity_route_sheet_operation'])
+is_operation = bool(config['DEFAULT']['operation'])
+is_department = bool(config['DEFAULT']['department'])
+is_equipment_class = bool(config['DEFAULT']['equipment_class'])
+is_entity_batch = bool(config['DEFAULT']['entity_batch'])
+is_user = bool(config['DEFAULT']['user'])
 
 datefrom = dt.datetime.strptime(str_date, '%Y-%m-%d').date()
 date_to = dt.datetime.strptime(str_date, '%Y-%m-%d') + timedelta(days=1)-timedelta(seconds=1)
 
-filter = "filter={{start_date ge %s}} and {{stop_date lt %s}}" % (datefrom.strftime('%Y-%m-%dT%H:%M:%S'), date_to.strftime('%Y-%m-%dT%H:%M:%S'))
+filter = "filter={{start_date ge %s}}" % (datefrom.strftime('%Y-%m-%dT%H:%M:%S'))
 
 path = os.path.abspath(os.path.dirname(sys.argv[0])) + '\\' + config['DEFAULT']['folder']
 if not os.path.exists(path):
@@ -35,36 +40,38 @@ c = req.cookies
 h = req.headers
 
 
+if is_entity:
+    responce = req.get(hostname+'/rest/collection/entity?order_by=id', cookies=c, headers=h)
+    j = json.loads(responce.text)
+    with open(path+'\entity.csv', 'w') as fout:
+        fout.write('id;identity;group;name;code\n')
+        if j['meta']['count'] != 0:
+            for item in j['entity']:
+                line = "%s;%s;%s;%s;%s\n" % (item['id'], item['identity'], item['group'], item['name'], item['code'])
+                fout.write(line)
 
-responce = req.get(hostname+'/rest/collection/entity?order_by=id', cookies=c, headers=h)
-j = json.loads(responce.text)
-with open(path+'\entity.csv', 'w') as fout:
-    fout.write('id;identity;group;name;code\n')
-    if j['meta']['count'] != 0:
-        for item in j['entity']:
-            line = "%s;%s;%s;%s;%s\n" % (item['id'], item['identity'], item['group'], item['name'], item['code'])
-            fout.write(line)
+if is_entity_route_sheet:
+    responce = req.get(hostname+'/rest/collection/entity_route_sheet?order_by=id&'+filter, cookies=c, headers=h)
+    j = json.loads(responce.text)
+    with open(path+ '\entity_route_sheet.csv', 'w') as fout:
+        fout.write('id;desc;identity;entity_batch_id;stop_date;note;start_date;type\n')
+        if j['meta']['count'] != 0:
+            for item in j['entity_route_sheet']:
+                line = "%s;%s;%s;%s;%s;%s;%s;%s\n" % (item['id'], item['desc'],item['identity'], item['entity_batch_id'], item['stop_date'], item['note'], item['start_date'], item['type'])
+                fout.write(line)
 
-responce = req.get(hostname+'/rest/collection/entity_route_sheet?order_by=id&'+filter, cookies=c, headers=h)
-j = json.loads(responce.text)
-with open(path+ '\entity_route_sheet.csv', 'w') as fout:
-    fout.write('id;desc;identity;entity_batch_id;stop_date;note;start_date;type\n')
-    if j['meta']['count'] != 0:
-        for item in j['entity_route_sheet']:
-            line = "%s;%s;%s;%s;%s;%s;%s;%s\n" % (item['id'], item['desc'],item['identity'], item['entity_batch_id'], item['stop_date'], item['note'], item['start_date'], item['type'])
-            fout.write(line)
-
-responce = req.get(hostname+'/rest/collection/entity_route_sheet_operation?order_by=id&'+filter, cookies=c, headers=h)
-j = json.loads(responce.text)
-with open(path+'\entity_route_sheet_operation.csv', 'w') as fout:
-    fout.write('id;calculation_identity;operation_id;equipment_id;equipment_class_id;stop_date;progress;department_id;note;calculation_session_id;start_date;executor_id;entity_route_sheet_id;status\n')
-    if j['meta']['count'] != 0:
-        for item in j['entity_route_sheet_operation']:
-            line = "%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s\n" % \
-                   (item['id'], item['calculation_identity'], item['operation_id'], item['equipment_id'], item['equipment_class_id'],
-                    item['stop_date'], item['progress'], item['department_id'], item['note'], item['calculation_session_id'],
-                    item['start_date'], item['executor_id'], item['entity_route_sheet_id'], item['status'])
-            fout.write(line)
+if is_entity_route_sheet_operation:
+    responce = req.get(hostname+'/rest/collection/entity_route_sheet_operation?order_by=id&'+filter, cookies=c, headers=h)
+    j = json.loads(responce.text)
+    with open(path+'\entity_route_sheet_operation.csv', 'w') as fout:
+        fout.write('id;calculation_identity;operation_id;equipment_id;equipment_class_id;stop_date;progress;department_id;note;calculation_session_id;start_date;executor_id;entity_route_sheet_id;status\n')
+        if j['meta']['count'] != 0:
+            for item in j['entity_route_sheet_operation']:
+                line = "%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s\n" % \
+                       (item['id'], item['calculation_identity'], item['operation_id'], item['equipment_id'], item['equipment_class_id'],
+                        item['stop_date'], item['progress'], item['department_id'], item['note'], item['calculation_session_id'],
+                        item['start_date'], item['executor_id'], item['entity_route_sheet_id'], item['status'])
+                fout.write(line)
 
 # print()
 # responce = req.get(hostname+'/rest/collection/executor', cookies=c, headers=h)
@@ -74,53 +81,58 @@ with open(path+'\entity_route_sheet_operation.csv', 'w') as fout:
 #         fout.write("{0}\n".format(item['identity']))
 #         print("{0}".format(item['identity']))
 
-responce = req.get(hostname+'/rest/collection/operation?order_by=id', cookies=c, headers=h)
-j = json.loads(responce.text)
-with open(path+'\operation.csv', 'w') as fout:
-    fout.write("id;nop;identity;name\n")
-    if j['meta']['count'] != 0:
-        for item in j['operation']:
-            line = line = "%s;%s;%s;%s\n" % (item['id'], item['nop'], item['identity'], item['name'])
-            fout.write(line)
+if is_operation:
+    responce = req.get(hostname+'/rest/collection/operation?order_by=id', cookies=c, headers=h)
+    j = json.loads(responce.text)
+    with open(path+'\operation.csv', 'w') as fout:
+        fout.write("id;nop;identity;name\n")
+        if j['meta']['count'] != 0:
+            for item in j['operation']:
+                line = line = "%s;%s;%s;%s\n" % (item['id'], item['nop'], item['identity'], item['name'])
+                fout.write(line)
 
-responce = req.get(hostname+'/rest/collection/department?order_by=id', cookies=c, headers=h)
-j = json.loads(responce.text)
-with open(path+'\department.csv', 'w') as fout:
-    fout.write('id;identity;name\n')
-    if j['meta']['count'] != 0:
-        for item in j['department']:
-            line = line = "%s;%s;%s\n" % (item['id'], item['identity'], item['name'])
-            fout.write(line)
+if is_department:
+    responce = req.get(hostname+'/rest/collection/department?order_by=id', cookies=c, headers=h)
+    j = json.loads(responce.text)
+    with open(path+'\department.csv', 'w') as fout:
+        fout.write('id;identity;name\n')
+        if j['meta']['count'] != 0:
+            for item in j['department']:
+                line = line = "%s;%s;%s\n" % (item['id'], item['identity'], item['name'])
+                fout.write(line)
 
-responce = req.get(hostname+'/rest/collection/equipment_class?order_by=id', cookies=c, headers=h)
-j = json.loads(responce.text)
-with open(path+'\equipment_class.csv', 'w') as fout:
-    fout.write('id;identity;name\n')
-    if j['meta']['count'] != 0:
-        for item in j['equipment_class']:
-            line = line = "%s;%s;%s\n" % (item['id'], item['identity'], item['name'])
-            fout.write(line)
+if is_equipment_class:
+    responce = req.get(hostname+'/rest/collection/equipment_class?order_by=id', cookies=c, headers=h)
+    j = json.loads(responce.text)
+    with open(path+'\equipment_class.csv', 'w') as fout:
+        fout.write('id;identity;name\n')
+        if j['meta']['count'] != 0:
+            for item in j['equipment_class']:
+                line = line = "%s;%s;%s\n" % (item['id'], item['identity'], item['name'])
+                fout.write(line)
 
-responce = req.get(hostname+'/rest/collection/entity_batch', cookies=c, headers=h)
-j = json.loads(responce.text)
-with open(path+'\eentity_batch.csv', 'w') as fout:
-    fout.write('id;calculation_identity;order_id;identity;parent_id;picked;from_state;entity_id;note;calculation_session_id;quantity;status\n')
-    if j['meta']['count'] != 0:
-        for item in j['entity_batch']:
-            line = line = "%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;\n" % \
-                          (item['id'], item['calculation_identity'], item['order_id'], item['identity'],item['parent_id'], item['picked'],
-                           item['from_state'], item['entity_id'], item['note'],item['calculation_session_id'], item['quantity'], item['status'])
-            fout.write(line)
+if is_entity_batch:
+    responce = req.get(hostname+'/rest/collection/entity_batch', cookies=c, headers=h)
+    j = json.loads(responce.text)
+    with open(path+'\eentity_batch.csv', 'w') as fout:
+        fout.write('id;calculation_identity;order_id;identity;parent_id;picked;from_state;entity_id;note;calculation_session_id;quantity;status\n')
+        if j['meta']['count'] != 0:
+            for item in j['entity_batch']:
+                line = line = "%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;\n" % \
+                              (item['id'], item['calculation_identity'], item['order_id'], item['identity'],item['parent_id'], item['picked'],
+                               item['from_state'], item['entity_id'], item['note'],item['calculation_session_id'], item['quantity'], item['status'])
+                fout.write(line)
 
-responce = req.get(hostname+'/rest/collection/user', cookies=c, headers=h)
-j = json.loads(responce.text)
-with open(path+'\\user.csv', 'w') as fout:
-    fout.write('id;disabled;identity;create_stamp;patronymic_name;last_name;name\n')
-    if j['meta']['count'] != 0:
-        for item in j['user']:
-            line = line = "%s;%s;%s;%s;%s;%s;%s\n" % (item['id'], item['disabled'], item['identity'], item['create_stamp'],
-                                                      item['patronymic_name'], item['last_name'], item['name'])
-            fout.write(line)
+if is_user:
+    responce = req.get(hostname+'/rest/collection/user', cookies=c, headers=h)
+    j = json.loads(responce.text)
+    with open(path+'\\user.csv', 'w') as fout:
+        fout.write('id;disabled;identity;create_stamp;patronymic_name;last_name;name\n')
+        if j['meta']['count'] != 0:
+            for item in j['user']:
+                line = line = "%s;%s;%s;%s;%s;%s;%s\n" % (item['id'], item['disabled'], item['identity'], item['create_stamp'],
+                                                          item['patronymic_name'], item['last_name'], item['name'])
+                fout.write(line)
 
 responce = req.get(hostname+'/action/logout', cookies=c, headers=h)
 print('Done.')
